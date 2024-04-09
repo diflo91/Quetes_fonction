024010e3d8a248d3e9b33c145ddc97bae8821e7a
diflo91 <rolsroyce796@gmail.com>
2024-04-09 13:49:20 +0200

    Ma Quetes boucle

---
+++ Quetes_boucle/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Quetes_boucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] moves = new String[32]; 
+
+            for (int i = 0; i < moves.Length; i++) 
+            
+            {
+
+                Console.WriteLine("Saisissez");
+                string pas = Console.ReadLine();
+                      
+                   if (pas == "done")
+                {
+                    break;
+                } else if (pas != "done")
+                {   
+                    moves[i] = pas;
+                    Console.WriteLine("Saisisser le nombre d'iteration");
+                    String iterationInput = Console.ReadLine();
+                    int iterationOutput = Convert.ToInt32(iterationInput);
+
+                    for (int y = 1; y < iterationOutput; y++)
+                    {
+                        Console.WriteLine("Saisissez");
+                        string pasDeux = Console.ReadLine();
+
+                        if (pasDeux == "done")
+                        {
+                            break;
+                        } else if (pasDeux != "done") { 
+
+                        moves[i] = pasDeux;
+                            Console.WriteLine($"La Danse saisi est : {moves[i]}");
+                    }
+
+                    }
+                    
+                    
+                }
+            }
+
+
+
+        }
+    }
+}
